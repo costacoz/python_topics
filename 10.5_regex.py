700f6a8226106d7b102412406f5e78a0f0d7bf8f
Konstantin <buserandi@gmail.com>
2020-07-08 21:28:41 +0100

    Added re module (regex) example.

--- 10.5_regex.py
+++ 10.5_regex.py
@@ -1,13 +1,13 @@
 """
-    Example of using Python's regex module.
+    example of using python's regex module.
 
-    When simple cases, highly recommended to use sting methods, like:
+    when simple cases, highly recommended to use sting methods, like:
     'tea for too'.replace('too', 'two')
 """
 
 import re
 
-regex = re.findall(r'[A-z]+', 'Luma&Buma, went around b00xes')
+regex = re.findall(r'[a-z]+', 'luma&buma, went around b00xes')
 print(regex)
 
 x = 'tea for too'.replace('too', 'two')
