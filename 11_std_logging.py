68244e60c9f724775b2d7fbfb5f29d401843f8d5
Konstantin <buserandi@gmail.com>
2020-07-11 14:35:29 +0100

    Added logging example.

---
+++ 11_std_logging.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.debug('This is debug logging message')
+logging.error('This is error logging message')
+logging.info('This is info logging message')
+logging.warning('This is warning logging message')
+logging.critical('This is critical logging message')
+
+logging.warning('Statistics \'%s\' file not found!', 'stats.data')
+
