42ef41362931c4d5151c3025329d0b0e6518f694
Konstantin <buserandi@gmail.com>
2020-07-14 22:44:45 +0100

    Large file read example.

---
+++ read_large_files.py
@@ -0,0 +1,21 @@
+"""
+    As written in official Python tutorial, when file size is too big
+    (>10Gb), then it's our problem to solve, if we want to read it.
+    If 10Gb file is in one line, then we can't even read it using readline.
+
+    So here comes handy this example, where it shows how to read large files
+    in blocks.
+    The idea is to use read method, that specifies fixed size of content
+    to be read from the file.
+"""
+
+from functools import partial
+
+
+def read_from_file(filename, block=1024 * 8):
+    """
+        Here partial() method is being used to optimize the code size.
+    """
+    with open(filename, "r") as fp:
+        for chunk in iter(partial(fp.read, block), ""):
+            yield chunk
